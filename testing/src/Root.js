fd07eb7c3765c4327285d2117b1698f22dcab30a
zorphe <jason.mueller92@gmail.com>
2019-06-18 15:33:58 -0700

    middleware added for data validation
    JSONSchema.net - used to generate the validation schema for tv4 (Tiny Validator) - seen in stateSchema.js;
    
    tv4 included into project;

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import async from './middlewares/async';
+import stateValidator from './middlewares/stateValidator';
 
 import reducers from 'reducers';
 
@@ -9,7 +10,10 @@ export default ({ children, initialState = {} }) => {
     const store = createStore(
         reducers,
         initialState,
-        applyMiddleware(async)
+        applyMiddleware(
+            async,
+            stateValidator
+        )
     );
     
     return (
