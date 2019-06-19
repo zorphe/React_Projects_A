379197cf43fbaa2d2d52c4507bf72c11000c7a5b
zorphe <jason.mueller92@gmail.com>
2019-06-19 01:03:06 -0700

    Added Redux;

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App>
-            <Route path="/" exact component={ Welcome } />
-            <Route path="/signup" component={ Signup } />
-        </App>
-    </BrowserRouter>,
+    <Provider store={ store }>
+        <BrowserRouter>
+            <App>
+                <Route path="/" exact component={ Welcome } />
+                <Route path="/signup" component={ Signup } />
+            </App>
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
 )
