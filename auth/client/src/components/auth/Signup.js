cf7d599cef2d52d4e0f8ef7c38fe53e5d9b36809
zorphe <jason.mueller92@gmail.com>
2019-06-19 00:13:48 -0700

    initial front-end files

---
+++ auth/client/src/components/auth/Signup.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Signup extends React.Component{
+    render(){
+        return(
+            <form>
+                <fieldset>
+                    <label>Email:</label>
+                </fieldset>
+                <fieldset>
+                    <label>Password:</label>
+                </fieldset>
+            </form>
+        );
+    }
+};
+
+export default Signup;
